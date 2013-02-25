b07640be5abd065ccecf129a0e83312118d6fb27
Pedro Teixeira <pedro.teixeira@gmail.com>
2013-02-25 16:46:35 +0000

    removed some logs

--- browser/js/todos.js
+++ browser/js/todos.js
@@ -10,7 +10,6 @@ function TodosCtrl($scope, websocket) {
 
     $scope.create =
     function create() {
-      console.log('create todo', $scope.newTodo);
       server.emit('new', $scope.newTodo);
       $scope.newTodo = {};
     };
@@ -25,18 +24,15 @@ function TodosCtrl($scope, websocket) {
 
     $scope.remove =
     function remove(todo) {
-      console.log('removing', todo);
       server.emit('remove', todo._id);
     };
 
     server.on('remove', function(todoId) {
-      console.log('server told me to remove', todoId);
       var found = -1;
       for(var i = 0; i < $scope.todos.length && found == -1; i++) {
         todo = $scope.todos[i];
         if (todo && todo._id == todoId) found = i;
       }
-      console.log('found:', found);
       if (found >= 0) {
         $scope.todos.splice(found, 1);
         $scope.$digest();
